c0dc5834b45fcb8370a1b17f3990d40fa1738e6c
altobellidardo <91988691+altobellidardo@users.noreply.github.com>
2022-11-13 14:20:17 -0300

    Update Bloques1.c

--- Bloques1.c
+++ Bloques1.c
@@ -8,27 +8,27 @@ notas y su promedio.
 */
 
 int main(void) {
-  float prom=0,aux=0; //promedio y auxiliar
-  int numeros=0;
-  char run = 1;
-  printf("Bienvenido\n");
-  while (run){
+    float prom=0,aux=0; //promedio y auxiliar
+    int numeros=0;
+    char run = 1;
+    printf("Bienvenido\n");
+    while (run){
     while (1){
-      printf("Ingrese su nota: ");
-      scanf("%f",&aux);
-      if (aux==-1)break;
-      else if (aux<0||aux>10)printf("[%.2f] no es valido\n",aux);
-      else{
-        printf("se agrego: %.2f\n",aux);
-        prom+=aux;
-        numeros++;
-      }
+        printf("Ingrese su nota: ");
+        scanf("%f",&aux);
+        if (aux==-1)break;
+        else if (aux<0||aux>10)printf("[%.2f] no es valido\n",aux);
+        else{
+            printf("se agrego: %.2f\n",aux);
+            prom+=aux;
+            numeros++;
+        }
     }
     if (numeros!=0)prom/=numeros;
     printf("el promedio es: %.2f con %d notas\n",prom,numeros);
     printf("Ingrese \n0 para salir\n1 para continuar\n");
     scanf("%d",&run);
     if (!run)printf("Gracias por usar el software");
-  }
-  return 0;
+    }
+    return 0;
 }
